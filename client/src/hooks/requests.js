a423a0beedeafddfccc2b905f793953535ea4183
Binyam Cheru <binyamcheru123@gmail.com>
2026-04-24 21:49:51 +0300

    feat: add launches feature with routes and model
    - Implemented launches model to store launch data.
    - Created launches controller to handle fetching all launches.
    - Added launches router to define API endpoint for launches.
    - Integrated launches router into main application.
    - Added morgan middleware for logging HTTP requests.

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,13 +1,18 @@
-const API_URL = "http://localhost:8000";
+const API_URL = 'http://localhost:8000';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 async function httpSubmitLaunch(launch) {
@@ -20,4 +25,9 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
