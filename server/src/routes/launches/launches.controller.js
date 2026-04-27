891f3b2213806c00e8e138865d2e1e1ec34d951f
Binyam Cheru <binyamcheru123@gmail.com>
2026-04-27 20:35:48 +0300

    Add new launch endpoint

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,20 @@
-const { getAllLaunches } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
 
 // why call this method to httpGetAllLaunchers beause it will directly deal with the http request and response objects, and it will be used as a callback function for the route handler in the routes file. This method will be responsible for sending the response back to the client with the list of all launches in JSON format.
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
 module.exports = {
   httpGetAllLaunches,
+  httpAddNewLaunch,
 };
