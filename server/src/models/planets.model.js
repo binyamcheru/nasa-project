4db83a28a943b3423430ffd33e2a411a9ac4ece7
Binyam Cheru <binyamcheru123@gmail.com>
2026-04-27 18:04:38 +0300

    Refactoring separation of concerns between the models and the controllers

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -40,7 +40,11 @@ function loadPlanetsData() {
   });
 }
 
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
 module.exports = {
+  getAllPlanets,
   loadPlanetsData,
-  planets: habitablePlanets,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,10 @@
-const { launches } = require('../../models/launches.model');
+const { getAllLaunches } = require('../../models/launches.model');
 
-function getAllLaunches(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
+// why call this method to httpGetAllLaunchers beause it will directly deal with the http request and response objects, and it will be used as a callback function for the route handler in the routes file. This method will be responsible for sending the response back to the client with the list of all launches in JSON format.
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLaunches());
 }
 
 module.exports = {
-  getAllLaunches,
+  httpGetAllLaunches,
 };
